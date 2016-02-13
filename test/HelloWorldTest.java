7ea0c3850dd1e8a3ed9c934cc283b33aee3ba6ac
FPNelson <FPNelson@users.noreply.github.com>
2016-02-12 18:55:18 -0600

    Create HelloWorldTest.java

---
+++ test/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+  @Test
+  public void testHelloWorld()
+  {
+    HelloWorld hello = new HelloWorld();
+    assertEquals("Hello World", hello.printHelloWorld());
+  }
+}
